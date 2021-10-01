93f9fbf11a15c29eb56f96e12ed95a14dab3f10d
Sjourney1 <alisuad0009@gmail.com>
2021-10-01 21:26:44 +0100

    commit

---
+++ .vscode/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    System.out.println("How many students will be added to the database?");
+    Scanner tellme = new Scanner ( System.in);
+    int numberofstudents = tellme.nextInt;
+    
+
+}
---
+++ .vscode/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+  public String studentName;	  		     	      	  	    	       	
+  public String studentYear;
+  public String course;	  		     	      	  	    	       	
+  public Int studentId;	  		     	      	  	    	       	
+  public int fees ;	  	
+  
+
+  
+}
