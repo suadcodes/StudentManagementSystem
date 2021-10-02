f11a6b8645e2e428fbe1cd38ad7244cb8d4abeae
Suad <suadcodes@gmail.com>
2021-10-02 22:04:40 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    System.out.println("How many students will be added to the database?");
+    Scanner tellme = new Scanner ( System.in);
+    int numberofstudents = tellme.nextInt;
+    
+}
